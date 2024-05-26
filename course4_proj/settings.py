f9f50d8d3940a2272099f5973f0b6cd6b2e6d55c
distance <from_a_distance@hispeed.ch>
2024-05-27 01:22:07 +0200

    initial

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
+this is my copy
+
 """
 
 from pathlib import Path
