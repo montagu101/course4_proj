4ed674b31ca43c8980c26727acaf5b380621e377
distance <from_a_distance@hispeed.ch>
2024-05-27 03:24:07 +0200

    Finish omdb api

--- manage.py
+++ manage.py
@@ -7,8 +7,15 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'course4_proj.settings')
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Dev") #--added to read configurations
     try:
-        from django.core.management import execute_from_command_line
+        #from django.core.management import execute_from_command_line
+        from configurations.management import execute_from_command_line
+
+
+
+
+    
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
